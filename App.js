f11a00879cf6b0a3f5242ab8651df215b02cc51a
Yuvraj3012 <71127295+Yuvraj3012@users.noreply.github.com>
2021-08-23 14:56:33 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,47 +1,103 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createSwitchNavigator,createAppContainer} from 'react-navigation'
-import Login from './screens/Login';
-import Landing from './screens/Landing';
-import Register from './screens/Register';
-import firebase from "firebase";
+import { View, Text } from 'react-native'
+
+import * as firebase from 'firebase'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2Q9_PRpqLQfqyyVzt1Ss6dfCoxGlAkwU",
-    authDomain: "instagram-6ce8d.firebaseapp.com",
-    projectId: "instagram-6ce8d",
-    storageBucket: "instagram-6ce8d.appspot.com",
-    messagingSenderId: "413581306942",
-    appId: "1:413581306942:web:466cb543abf1fe1cefb56f"
-  };
+  apiKey: "AIzaSyAKFIwBIGgMBMh2c6io0GAkqE8-Ft2ri9w",
+  authDomain: "instagram-b8004.firebaseapp.com",
+  projectId: "instagram-b8004",
+  storageBucket: "instagram-b8004.appspot.com",
+  messagingSenderId: "747663622108",
+  appId: "1:747663622108:web:a1962968f7c336c32c5919"
+};
 
-  if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig)
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
+import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
+import CommentScreen from './components/main/Comment'
+
+
+const Stack = createStackNavigator();
+
+
+export class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      loaded: false,
+    }
   }
 
-export default class App extends React.Component {
- render() {
-    return <AppContainer />;
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <NavigationContainer >
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Comment" component={CommentScreen} navigation={this.props.navigation}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    )
   }
 }
 
-
-const SwitchNavigator= createSwitchNavigator({
-  Landing : Landing,
-  Register:Register,
-  Login : Login
-
-
-})
-
-const AppContainer = createAppContainer(SwitchNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
